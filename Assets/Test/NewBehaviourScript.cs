47e919829fa73f39608be3c9af5994466bccce0c
Bomb-X <bombix34@gmail.com>
2020-07-27 10:23:04 +0200

    initial Unity Commit

---
+++ Assets/Test/NewBehaviourScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+	public FafaFloat flo;
+	void Start(){
+		//flo.setVariable(3f);
+	} 
+
+	void Update(){
+		print(flo.getVariable());
+	}
+}
---
+++ Assets/Test/NewBehaviourScript1.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript1 : MonoBehaviour {
+	public SimpleMusic sm;
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(sm.StartMusic(GetComponent<AudioSource>()));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
